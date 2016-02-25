d4fb9d5666f3453dd5e0ffeca8493feb9e5f3972
Søren Louv-Jansen <slo@tradeshift.com>
2016-02-25 15:41:09 +0100

    Added getBuddyList

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ fbSleep.getLastActiveTimes = function(config) {
         url: 'https://www.messenger.com',
         jar: getCookieJar(config, 'https://www.messenger.com'),
         gzip: true,
-        method: 'GET',
         headers: {
             'User-Agent': 'curl/7.43.0'
         }
@@ -39,6 +38,42 @@ fbSleep.getLastActiveTimes = function(config) {
     .then(function(body) {
         var lastActiveTimes = JSON.parse(body.match(/lastActiveTimes\":({.+?})/)[1]);
         return lastActiveTimes;
+    });
+};
+
+// curl 'https://5-edge-chat.facebook.com/pull?channel=p_<user_id>&seq=1&partition=-2&cb=jeih&idle=1&qp=y&cap=8&pws=fresh&isq=57540&msgs_recv=0&uid=<user_id>&viewer_uid=<user_id>&sticky_token=93&sticky_pool=frc3c09_chat-proxy&state=active' -H 'Cookie: c_user=<user_id>;xs=<xs>;'
+fbSleep.getBuddyList = function(config) {
+    return request({
+        url: 'https://5-edge-chat.facebook.com/pull',
+        jar: getCookieJar(config, 'https://5-edge-chat.facebook.com'),
+        qs: {
+            channel: 'p_' + config.c_user,
+            seq: 1,
+            partition: -2,
+            cb: 'jeih',
+            idle: 1,
+            qp: 'y',
+            cap: 8,
+            pws: 'fresh',
+            isq: 57540,
+            msgs_recv: 0,
+            uid: config.c_user,
+            viewer_uid: config.c_user,
+            sticky_token: 93,
+            sticky_pool: 'frc3c09_chat-proxy',
+            state: 'active'
+        },
+        gzip: true,
+        headers: {
+            'User-Agent': 'curl/7.43.0'
+        }
+    })
+    .then(function(body) {
+        var buddyList = JSON.parse(body.replace('for (;;);', '')).ms[0].buddyList;
+        return _.reduce(buddyList, function(memo, user, userId) {
+            memo[userId] = user.lat;
+            return memo;
+        }, {});
     });
 };
 
@@ -78,26 +113,43 @@ fbSleep.fetchActiveUsers = function(config) {
 fbSleep.getRecentlyActiveUsers = function(config, since) {
     validateConfig(config);
 
-    var activeUsersRequest = fbSleep.fetchActiveUsers(config);
-    var lastActiveTimesRequest = fbSleep.getLastActiveTimes(config);
+    return fbSleep.getBuddyList(config)
+        .then(function(users) {
+            if (users.length === 0) {
+                throw new Error('No users found');
+            }
 
-    return Bluebird.all([activeUsersRequest, lastActiveTimesRequest])
-        .spread(function(activeUsers, lastActiveTimes) {
-            var users = _.merge(activeUsers, lastActiveTimes);
+            return formatUsers(users, since);
+        })
 
-            return _(users)
-                .toPairs()
-                .map(function(user) {
-                    return {
-                        userId: user[0],
-                        timestamp: user[1] * 1000
-                    };
-                })
-                .filter(function(user) {
-                    return user.timestamp >= since;
-                })
-                .value();
+        // Fallback to other way of retriving users
+        .catch(function(e) {
+            console.log('An error occured in buddyList', e);
+            var activeUsersRequest = fbSleep.fetchActiveUsers(config);
+            var lastActiveTimesRequest = fbSleep.getLastActiveTimes(config);
+
+            return Bluebird.all([activeUsersRequest, lastActiveTimesRequest])
+               .spread(function(activeUsers, lastActiveTimes) {
+                   var users = _.merge(activeUsers, lastActiveTimes);
+
+                   return formatUsers(users, since);
+               });
         });
 };
+
+function formatUsers(users, since) {
+    return _(users)
+        .toPairs()
+        .map(function(user) {
+            return {
+                userId: user[0],
+                timestamp: user[1] * 1000
+            };
+        })
+        .filter(function(user) {
+            return user.timestamp >= since;
+        })
+        .value();
+}
 
 module.exports = fbSleep;
---
+++ mocks/getBuddyList.js
@@ -0,0 +1,1 @@
+module.exports = 'for (;;); {"t":"msg","seq":5,"u":123456789,"ms":[{"type":"chatproxy-presence","buddyList":{"1234":{"lat":1456410577},"2345":{"lat":1456400101},"6789":{"lat":1456410490}}}]}';
